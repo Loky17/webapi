ba4c064b8bd6ffd1f5c1cd541d36dc31af131467
Simone Martellucci <simomarte30@gmail.com>
2024-03-12 20:42:25 +0100

    first commit

---
+++ Project1/LOLService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json; 
+using RiotSharp;
+using RiotSharp.Endpoints.SummonerEndpoint;
+using RiotSharp.Interfaces;
+using RiotSharp.Misc;
+using System;
+
+
+namespace Project1
+{
+    public class LOLService
+    {
+        RiotApi Api { get; set; }
+        public LOLService()
+        {
+            Api = RiotApi.GetDevelopmentInstance(File.ReadAllText("ApiKey/Apikey.ini"));
+        }
+        public async Task<Summoner> GetSummonerAsync(string username, Region region)
+        {
+            Summoner nome = new();
+            return await Api.Summoner.GetSummonerByNameAsync(region,username);
+        }
+    }
+}
